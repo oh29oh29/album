2940007478a6d97e21e8a34fc5edf2912387291a
oh29oh29 <hyukjae89@gmail.com>
2019-02-06 16:59:49 +0900

    포스트 임시 목데이터 추가

--- src/main/java/pe/oh29oh29/album/controller/PostController.java
+++ src/main/java/pe/oh29oh29/album/controller/PostController.java
@@ -1,23 +1,21 @@
 package pe.oh29oh29.album.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
+import pe.oh29oh29.album.service.PostService;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
 
+    @Autowired
+    private PostService postService;
+
     @GetMapping("")
-    public ResponseEntity<?> getPosts() {
-        List<String> posts = new ArrayList();
-        posts.add("a");
-        posts.add("b");
-        posts.add("c");
-        return ResponseEntity.ok(posts);
+    public ResponseEntity<?> getPosts(String categoryId) {
+        return ResponseEntity.ok(postService.getPosts());
     }
 }
---
+++ src/main/java/pe/oh29oh29/album/model/Post.java
@@ -0,0 +1,23 @@
+package pe.oh29oh29.album.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+public class Post {
+
+    @Id
+    private String id;
+
+    @Column
+    private String description;
+
+    @Column
+    private String date;
+}
